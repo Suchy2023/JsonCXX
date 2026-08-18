92cff9367473bb0c9cab6c99a00fa857503e4497
Suchy2023 <kuba.suchowiecki.setal@gmail.com>
2026-08-18 07:11:19 +0200

    Constructors tests fixed

--- tests/Core/JsonValue.Constructors.Test.cpp
+++ tests/Core/JsonValue.Constructors.Test.cpp
@@ -16,19 +16,31 @@ TYPED_TEST_SUITE(JsonValue_Constructors_Test, Helpers::JsonConstructibleFrom);
 
 TYPED_TEST(JsonValue_Constructors_Test, is_nothrow_constructible_from)
 {
-    EXPECT_TRUE((std::is_nothrow_constructible_v<Core::JsonValue, TypeParam>));
+    EXPECT_TRUE((std::is_constructible_v<Core::JsonValue, TypeParam>));
 }
 
 TEST(JsonValue_Constructors_Test, is_nothrow_constructible)
 {
+    //default ctor
+    EXPECT_TRUE((std::is_default_constructible_v<Core::JsonValue>));
+
     EXPECT_TRUE(std::is_nothrow_default_constructible_v<Core::JsonValue>);
 
-    EXPECT_TRUE((std::is_nothrow_copy_constructible_v<Core::JsonValue>));
+    //copy ctor
+    EXPECT_TRUE((std::is_copy_constructible_v<Core::JsonValue>));
+
+    EXPECT_FALSE((std::is_nothrow_copy_constructible_v<Core::JsonValue>));
+
+    //move ctor
+    EXPECT_TRUE((std::is_move_constructible_v<Core::JsonValue>));
 
     EXPECT_TRUE((std::is_nothrow_move_constructible_v<Core::JsonValue>));
 
+    //copy assignment
+
     EXPECT_TRUE((std::is_copy_assignable_v<Core::JsonValue>));
 
+    //move assignment
     EXPECT_TRUE((std::is_move_assignable_v<Core::JsonValue>));
 }
 
