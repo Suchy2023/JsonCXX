19a95b5af3fdb8a8ccdb616568c4ab4fff25fad8
Suchy2023 <kuba.suchowiecki.setal@gmail.com>
2026-08-23 18:18:29 +0200

    Added more parser errors

--- include/JsonC++/JsonParser/JsonParserError.h
+++ include/JsonC++/JsonParser/JsonParserError.h
@@ -6,7 +6,10 @@ namespace JsonParser
 
     enum class JsonParserError : unsigned char
     {
-        parse_error
+        parse_error,
+        out_of_range,
+        not_integral,
+        invalid_format
     };
 
 }
