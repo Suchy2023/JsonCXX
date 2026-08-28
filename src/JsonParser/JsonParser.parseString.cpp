59cc9ce707dd26708ae716569987f7a4eaf03f4e
Suchy2023 <kuba.suchowiecki.setal@gmail.com>
2026-08-28 17:53:18 +0200

    removed not used at the moment

--- src/JsonParser/JsonParser.parseString.cpp
+++ src/JsonParser/JsonParser.parseString.cpp
@@ -5,17 +5,5 @@ std::string JsonParser::parseString(const char *&iterator, const char *const &en
 
   std::string result{};
 
-  bool stringStarted = true;
-
-  for (; iterator != end; ++iterator) {
-    if (*iterator == 34) {
-      stringStarted = true;
-      continue;
-    }
-
-    result += *iterator;
-  }
-
-  return std::move(result);
 };
 } // namespace JsonParser
